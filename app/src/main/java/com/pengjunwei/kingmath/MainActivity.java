311eee3aa4d13d0d29d478e1f20b598d3b272572
wiki <wiki.peng@qq.com>
2017-03-10 15:06:58 +0800

    初始化工程

---
+++ app/src/main/java/com/pengjunwei/kingmath/MainActivity.java
@@ -0,0 +1,17 @@
+package com.pengjunwei.kingmath;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by WikiPeng on 2017/3/10 14:58.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
